5e0d3b14be02b021f0efeea39a1d12119aac9f7c
jota7337 <jorgeyate37@gmail.com>
2024-11-24 01:56:46 -0500

    fix: change dashboard in samples page with  clientId

--- src/app/dashboard/companies/page.tsx
+++ src/app/dashboard/companies/page.tsx
@@ -2,10 +2,8 @@ import { Suspense } from "react"
 import { getClients } from "@/lib/services"
 import { TableCompanies } from "@/ui/dashboard/companies/table"
 import { TableCompaniesProps } from "@/lib/@types/props"
-import { auth } from "@/lib/auth"
 
 const DashboardCompaniesPage = async () => {
-    const session = await auth()
     const companies = await getClients<TableCompaniesProps["companies"]>()
 
     return (
