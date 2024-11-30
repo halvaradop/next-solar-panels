e21721274abc5c5f8ab0b72a34cee6a83d6790fa
jota7337 <jorgeyate37@gmail.com>
2024-11-30 02:35:37 -0500

    fix: remove proof from pdf

--- src/app/dashboard/pdf/page.tsx
+++ src/app/dashboard/pdf/page.tsx
@@ -3,7 +3,6 @@ const pdf = async () => {
     return (
         <div className="flex justify-between mt-2">
             <h2 className="text-xl font-semibold mb-2">Intento de Pdf</h2>
-            <DowloandBotton></DowloandBotton>
         </div>
     )
 }
