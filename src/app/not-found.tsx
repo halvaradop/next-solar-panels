a7d250ca8a142c9c7088020d5d5a6e2881be0eba
Hernan Alvarado <hernanvid123@gmail.com>
2024-12-05 23:00:58 -0500

    chore: add symbols

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,8 +8,8 @@ const NotFound = () => {
         <main className="min-h-main flex items-center justify-center flex-col text-center">
             <h1 className="fluency-4xl font-bold">Page Not Found</h1>
             <p className="mt-2 mb-6 text-lg">
-                Oops! The page you're looking for can't be found. We're working on it and it will be available soon. Please check
-                back later.
+                Oops! The page you&apos;re looking for can&apos;t be found. We&apos;re working on it and it will be available
+                soon. Please check back later.
             </p>
             <Image src={websiteBuilding} alt="Website under construction" width={400} height={300} priority />
             <Button className="mt-6 mx-auto" asChild>
