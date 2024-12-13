44ffd1db9c7b866cbc5d8d26dab2118a532c31f9
jota7337 <jorgeyate37@gmail.com>
2024-12-13 03:51:33 -0500

    fix: changes login

--- src/ui/dashboard/pick-project/project.tsx
+++ src/ui/dashboard/pick-project/project.tsx
@@ -8,7 +8,7 @@ export const Project = ({ idProject, designation }: ProjectOnPickProps) => {
         <li
             className="bg-white p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-300"
             key={idProject}
-            onClick={async () => await setCookieToken(idProject)}
+            onClick={async () => await setCookieToken("c57111f6-b91a-11ef-9da8-020168ba9d14")}
         >
             <Link href="/dashboard/projects" className="block h-full text-center">
                 <div className="text-xl font-semibold text-slate-700">{designation}</div>
